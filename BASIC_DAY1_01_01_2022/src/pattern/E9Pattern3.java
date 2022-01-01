cbe2436085322bd4a213a27bf0e8ded5bdbf3789
--get <coursesonline33@gmail.com>
2022-01-01 21:36:38 +0530

    D1S2

---
+++ BASIC_DAY1_01_01_2022/src/pattern/E9Pattern3.java
@@ -0,0 +1,22 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class E9Pattern3 {
+
+	public static Scanner sc;
+	public static int rows;
+	
+	public static void main(String[] args) {
+		System.out.println("Enter number of rows: ");
+		sc = new Scanner(System.in);
+		rows = sc.nextInt();
+		for(int i=0; i< rows; i++) {
+			for(int j=i; j<rows; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
