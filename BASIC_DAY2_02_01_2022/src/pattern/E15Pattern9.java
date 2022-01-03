d5c2a3d86392862cfe11433a30df275e38f0dc04
--get <coursesonline33@gmail.com>
2022-01-03 09:25:35 +0530

    D2S2-LATE

---
+++ BASIC_DAY2_02_01_2022/src/pattern/E15Pattern9.java
@@ -0,0 +1,34 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class E15Pattern9 {
+
+	public static Scanner sc;
+	public static int rows;
+	
+	public static void main(String[] args) {
+		System.out.println("Enter number of rows: ");
+		sc = new Scanner(System.in);
+		rows = sc.nextInt();
+		int rowCounter = 1;
+		int nst = 1;
+		int nsp = (rows * 2) - 1;
+		while(rowCounter <= rows) {
+			for(int i=0; i<nst; i++) {
+				System.out.print("*");
+			}
+			for(int i=0; i<nsp; i++) {
+				System.out.print(" ");
+			}
+			for(int i=0; i<nst; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			nst += 1;
+			nsp -= 2;
+			rowCounter++;
+		}
+	}
+
+}
